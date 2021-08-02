59f6cee43d88ed435cddf88bd8430f5128956c2e
7wc98#14 <2923616405@qq.com>
2021-08-02 20:52:37 +0800

    update

--- app/src/main/java/com/pyc/empty/MainActivity.java
+++ app/src/main/java/com/pyc/empty/MainActivity.java
@@ -3,6 +3,7 @@ package com.pyc.empty;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,16 +25,20 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn = findViewById(R.id.btn_click);
         /*点击监听器*/
-        btn.setOnClickListener(v -> Log.e(TAG, "onclick"));
+        /*btn.setOnClickListener(v -> Log.e(TAG, "onclick"));*/
 
         /*长按监听器*/
         btn.setOnLongClickListener(v -> {
             Log.e(TAG, "onLongClick");
-            return false;
+            return true;
         });
         btn.setOnTouchListener((v, event) -> {
             Log.e(TAG, "onTouch" + event.getAction());
             return false;
         });
+    }
+
+    public void PClick(View view) {
+        Log.e(TAG, "onClick");
     }
 }
