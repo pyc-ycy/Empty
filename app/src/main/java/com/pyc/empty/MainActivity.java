d18bb1fd4fce29266d149b41285cea48acc5d70d
7wc98#14 <2923616405@qq.com>
2021-09-04 18:03:55 +0800

    learning how to design the animation list(逐帧动画）

--- app/src/main/java/com/pyc/empty/MainActivity.java
+++ app/src/main/java/com/pyc/empty/MainActivity.java
@@ -2,11 +2,10 @@ package com.pyc.empty;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.util.Log;
-
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +16,7 @@ import java.util.List;
 public class MainActivity extends Activity {
 
     private static final String TAG = "pyc:";
+    private boolean stop_flag = true;
 
     /*list view*/
     private List<Bean> data = new ArrayList<>();
@@ -26,32 +26,22 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.recycle_view);
-        /*list view and recycle view*/
-        for (int i=0; i<100; i++) {
-            Bean bean = new Bean();
-            bean.setName("item: " + i);
-            data.add(bean);
-        }
-        RecyclerView recyclerView = findViewById(R.id.rv);
-        /*布局1*/
-        /*LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);*/
-        /*布局2*/
-        /*GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
-        recyclerView.setLayoutManager(gridLayoutManager);*/
-        /*layout 3*/
-        StaggeredGridLayoutManager staggeredGridLayoutManager =
-                new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
-        recyclerView.setLayoutManager(staggeredGridLayoutManager);
-        RecycleViewAdapter adapter = new RecycleViewAdapter(data, this);
-        recyclerView.setAdapter(adapter);
-        /*单击监听*/
-        adapter.setOnItemClickListener(new RecycleViewAdapter.OnRecyclerItemClickListener() {
+        setContentView(R.layout.animation_list);
+
+        RelativeLayout relativeLayout = findViewById(R.id.animation_list_layout);
+        AnimationDrawable anim = (AnimationDrawable) relativeLayout.getBackground();
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onRecyclerItemClick(int position) {
-                Log.e(TAG, "item click on: " + position);
+            public void onClick(View v) {
+                if (stop_flag) {
+                    anim.start();
+                    stop_flag = false;
+                } else {
+                    anim.stop();
+                    stop_flag = true;
+                }
             }
         });
+
     }
 }
