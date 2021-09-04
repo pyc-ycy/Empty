2ca78ad6267c482a362a00a9e76413f34e3b7b1d
7wc98#14 <2923616405@qq.com>
2021-09-04 14:25:24 +0800

    learning how to design the list view

--- app/src/main/java/com/pyc/empty/MainActivity.java
+++ app/src/main/java/com/pyc/empty/MainActivity.java
@@ -3,6 +3,13 @@ package com.pyc.empty;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author 彭友聪
@@ -12,13 +19,31 @@ public class MainActivity extends Activity {
 
     private static final String TAG = "pyc:";
 
+    /*list view*/
+    private List<Bean> data = new ArrayList<>();
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.constrain_layout);
+        setContentView(R.layout.list_view);
+        /*list view*/
+        for (int i=0; i<100; i++) {
+            Bean bean = new Bean();
+            bean.setName("item: " + i);
+            data.add(bean);
+        }
+
+        ListView listView = findViewById(R.id.lv);
+        listView.setAdapter(new MyAdapter(data, this));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.e(TAG, "onItemClick: " + position);
+            }
+        });
+
     }
 
 }
